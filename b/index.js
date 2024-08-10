61c92824d4f23597d152f3ae8c958a8f164c85dd
mamawski01 <72120572+mamawski01@users.noreply.github.com>
2024-08-10 18:27:42 +0800

    okay

---
+++ b/index.js
@@ -0,0 +1,30 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import mongoose from "mongoose";
+import http from "http";
+
+import router from "./src/routes/routes.js";
+
+dotenv.config();
+const PORT = process.env.PORT || process.env.API_PORT;
+const localhost = `http://localhost:${PORT}/`;
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const server = http.createServer(app);
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    server.listen(PORT, () => {
+      console.log(localhost + " connected to db");
+    });
+  })
+  .catch((err) => {
+    console.log("Db and server fail" + err);
+  });
+
+app.use("/", router);
